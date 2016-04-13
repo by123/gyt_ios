ebfe4b32875d1b935c7ac29c794ae2525c6a1452
老黄 <by.huang@byhuangdemacbook-pro.local>
2016-04-13 14:58:52 +0800

    Initial Commit

---
+++ gyt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gyt
+//
+//  Created by by.huang on 16/4/13.
+//  Copyright © 2016年 by.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gyt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gyt
+//
+//  Created by by.huang on 16/4/13.
+//  Copyright © 2016年 by.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
